e65247028bec829ecc80b77873f911daefbf25ed
korablik <jungwoo07191@naver.com>
2026-02-20 16:27:31 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a==b && b==c) printf("%d", 10000+a*1000);
+    else if(a==b || a==c) printf("%d", 1000+a*100);
+    else if(b==c) printf("%d", 1000+b*100);
+    else printf("%d", ((a>b?a:b)>c?(a>b?a:b):c) * 100);
+    return 0;
+}

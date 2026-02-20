326409a1656bc2dacf34de7d90aea29498ceb914
korablik <jungwoo07191@naver.com>
2026-02-20 16:17:20 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    b+=c;
+    a+=b/60;
+    b%=60;
+    a%=24;
+    printf("%d %d", a, b);
+    return 0;
+}

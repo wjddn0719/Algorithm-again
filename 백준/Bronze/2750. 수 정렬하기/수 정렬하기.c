c7a10e06df8e173146f3baae7b943ee249caab3a
korablik <jungwoo07191@naver.com>
2026-02-24 23:47:35 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    short N, map[1000]={0};
+    scanf("%hd", &N);
+    for(int i=0; i<N; i++){
+        scanf("%hd", (map+i));
+    }
+    for(int i=0; i<N-1; i++){
+        for(int j=i+1; j<N; j++){
+            if(map[i] > map[j]){
+                short tmp = map[i];
+                map[i] = map[j];
+                map[j] = tmp;
+            }
+        }
+    }
+    for(int i=0; i<N; i++){
+        printf("%d\n", map[i]);
+    }
+    return 0;
+}
